2fcf66adfe55b8971e0bee874cc8e661a2b304f8
ZackAkil <zackakil94@gmail.com>
2016-03-13 18:16:20 +0000

    Added my solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: creates a list of accessor functions for a matrix and its inverse
+## that exist within a different environment.
+## cacheSolve: returns the stored inverse of the matrix stored in the different 
+## environment created using the makeCacheMatrix function and if it hasn't been 
+## been calculated yet: calculates it and stores it using the accessor functions.
 
-## Write a short comment describing this function
+## Takes a matrix as a parameter with a default value of a empty matrix
+## returns a list of getter and setter functions for the matrix and
+## its inverse which use lexical scoping to access the variables declared
+## within the [makeCacheMatrix] function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  s <- NULL
+  set <- function(y) {
+    x <<- y 
+    s <<- NULL
+  }
+  get <- function() x
+  setSolve <- function(solve) s <<- solve
+  getSolve <- function() s
+  
+  list(set = set, get = get,
+       setSolve = setSolve,
+       getSolve = getSolve)
 }
 
 
-## Write a short comment describing this function
+## Takes a special list object (produced by the makeCacheMatrix function)
+## along with any extra arguments you want to pass down to the solve function
+## returns the special object's inverse, if the object dosn't have a value for 
+## the inverse: it will be calculated and saved inside the special object then return 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  s <- x$getSolve()
+  if(!is.null(s)) {
+    message("getting cached data")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setSolve(s)
+  s
 }
+
